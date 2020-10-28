5b5258ddef3d2e1a2f7aac99788455643b308ecc
bminczu <bminczu@gmail.com>
2020-10-28 15:37:26 -0500

    commit tree 3e6056008345fb091216f445febb7ced47fe5079

--- lib/person.rb
+++ lib/person.rb
@@ -1,1 +1,33 @@
 # your code goes here
+class Person
+    def initialize(name, balance = 25, happiness = 8, hygine =8 )
+        @name = name
+        @balance = balance
+        @happiness = happiness
+        @hygine = hygine
+    end
+    def name
+        @name 
+    end
+    def bank_account
+        @balance 
+    end
+    def bank_account=(new_balance)
+        @balance = new_balance
+    end
+
+    def happiness
+        @happiness 
+    end
+
+    def happiness
+        @happiness 
+    end
+  
+    def happy? > 7
+        @happiness
+    end
+end
+
+
+
